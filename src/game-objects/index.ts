4e2bb1743783d37716e8eede4f0b05cfe153f1d7
quangvd2 <quangvd2@vng.com.vn>
2026-01-26 15:03:59 +0700

    chore: remove legacy src folder after monorepo migration

--- src/game-objects/index.ts
+++
@@ -1,4 +0,0 @@
-export { GameObject } from './game-object';
-export { Component } from './component';
-export { ComponentManager } from './component-manager';
-export type { ComponentClass, GameObjectConfig } from '../types/game-object.types';
